1a74c5b1037670ed376d6a88ff297a005c18f290
Silvia <silvia.garbas@gmail.com>
2023-06-12 20:05:14 +0200

    Initial commit

---
+++ src/controllers/sample.controller.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from 'express';
+import { SampleRepo } from '../repository/sample.repository.js';
+import createDebug from 'debug';
+const debug = createDebug('W6:SampleController');
+
+export class SampleController {
+  // eslint-disable-next-line no-unused-vars
+  constructor(private repo: SampleRepo) {
+    debug('Instantiated SampleController');
+    debug(this.repo);
+  }
+
+  async getAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.send(await this.repo.query());
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.send(await this.repo.queryById(req.params.id));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async post(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.status(201);
+      res.send(await this.repo.create(req.body));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async patch(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.status(202);
+      res.send(await this.repo.update(req.params.id, req.body));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteById(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.status(204);
+      res.send(await this.repo.delete(req.params.id));
+    } catch (error) {
+      next(error);
+    }
+  }
+}
---
+++ src/routers/sample.router.ts
@@ -0,0 +1,15 @@
+import { Router as createRouter } from 'express';
+import { SampleController } from '../controllers/sample.controller.js';
+import { SampleRepo } from '../repository/sample.repository.js';
+import { Repo } from '../repository/repo.js';
+import { Sample } from '../entities/sample.js';
+
+const repo: Repo<Sample> = new SampleRepo();
+const controller = new SampleController(repo);
+export const sampleRouter = createRouter();
+
+sampleRouter.get('/', controller.getAll.bind(controller));
+sampleRouter.get('/:id', controller.getById.bind(controller));
+sampleRouter.post('/', controller.post.bind(controller));
+sampleRouter.patch('/:id', controller.patch.bind(controller));
+sampleRouter.delete('/:id', controller.deleteById.bind(controller));
